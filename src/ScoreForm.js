2a148a103882025bd1237b82c42d2e98272fb902
katiek78 <kermodekatie@gmail.com>
2025-11-08 16:52:12 +0000

    sort users alphabetically in ScoreForm

--- src/ScoreForm.js
+++ src/ScoreForm.js
@@ -151,6 +151,11 @@ const ScoreForm = ({ onSubmitScore, form, editing, competitionId }) => {
                 ?.filter(
                   (usr) => competitionData.compUsers.indexOf(usr._id) > -1
                 )
+                .sort((a, b) => {
+                  const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
+                  const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
+                  return nameA.localeCompare(nameB);
+                })
                 .map((usr) => (
                   <option key={usr._id} value={usr._id}>
                     {usr.firstName} {usr.lastName}
